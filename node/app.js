8cfab37c19d1b08236924a1d0f557ffb262d20eb
Steven Lam <lam.steven7@gmail.com>
2019-02-10 17:16:23 -0500

    Added request from node to java server

--- node/app.js
+++ node/app.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const fetch = require('node-fetch');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello STEVEN, my old friend.'));
+app.get('/', (req, res) => { 
+    fetch('http://java:4000/greeting')
+        .then(res => res.json())
+        .then(json => res.send('id: ' + json.id))
+        .catch(err => console.log(err));
+});
 
 app.listen(port, () => 'Listening to server Marcus');
