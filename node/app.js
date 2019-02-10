b4b75307d698f5b09aefc16cb1e8d445683c5041
Steven Lam <lam.steven7@gmail.com>
2019-02-10 16:07:57 -0500

    Added initial java server

--- node/app.js
+++ node/app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello Marcus, my old friend.'));
+app.get('/', (req, res) => res.send('Hello STEVEN, my old friend.'));
 
 app.listen(port, () => 'Listening to server Marcus');
